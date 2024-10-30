02afb4319bddf56f6317d269dae9188b7299a8a2
izziedelph <ham.isma@yahoo.com>
2024-10-30 00:04:43 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_instance" "github_tests" {
+    ami                    = "ami-0e86e20dae9224db8"
+    instance_type          = "t2.micro"
+
+
+    tags = {
+        Name = "github-tests"
+    }
+}
