4e939f7e27123562e9f56356e7f1a24afb42e889
izziedelph <ham.isma@yahoo.com>
2024-10-30 08:56:32 +0000

    Initial commit

--- main.tf
+++ main.tf
@@ -2,13 +2,21 @@ provider "aws" {
     region = "us-east-1"
 }
 
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
 resource "aws_instance" "github_tests" {
-    ami                    = "ami-0e86e20dae9224db8"
-    instance_type          = "t2.micro"
-
-
+    ami           = "ami-0e86e20dae9224db8"
+    instance_type = "t2.micro"
     tags = {
         Name = "github-tests"
+        ManagedBy = "Terraform"
     }
 }
